3e6104f320eccc033b96c7943c3a1189cdd4555e
Chris Raff <crharfifs@gmail.com>
2017-06-05 23:44:53 -0500

    Canvas on webpage, also parseDays works correctly

--- SWFT/swft.js
+++ SWFT/swft.js
@@ -38,10 +38,14 @@ function parseDays(string) {
 	var out = [false, false, false, false, false];
 	var map = { "Mo":0, "Tu":1, "We":2, "Th":3, "Fr":4 };
 	var i = 0;
-	while (string.length < i+2) {
+	console.log(string);
+	while (string.length >= i+2) {
 		str = string.substring(i, i+2);
+		console.log(str);
 		out[map[str]] = true;
+		i+=2;
 	}
+	console.log(out);
 	return out;
 }
 function parseTime(string) {
@@ -187,9 +191,123 @@ function parseAndPrint(text) {
 	console.log('beginning');
 	var cl = parseSchedule(text);
 	console.log('completed--------------');
-	for (var i = 0; i < cl.length; i++) {
-		cl[i].log();
-	}
+	//for (var i = 0; i < cl.length; i++) {
+	//	cl[i].log();
+	//}
 	//console.log(cl);
+	updateCanvas(document.getElementById('image'), cl);
 	return false;
+}
+
+//hue is out of 1
+function hueToRed(hue) {
+	hue %= 1;
+	if (hue < 0) {
+		hue += 1;
+	}
+	return Math.floor(255*Math.max(0, 3*(Math.abs(0.5-hue)-1/6)));
+}
+function hueVal(h, v) {
+	r = hueToRed(h);
+	g = hueToRed(h-1/3);
+	b = hueToRed(h-2/3);
+	if (v < 0.5) {
+		r *= v*2;
+		g *= v*2;
+		b *= v*2;
+	} else {
+		r = 1 - (1 - r)*(1-v)*2;
+		g = 1 - (1 - g)*(1-v)*2;
+		b = 1 - (1 - b)*(1-v)*2;
+	}
+	return '#' + r.toString(16) + g.toString(16) + b.toString(16);
+}
+
+function padMinutes(number) {
+	var numAsString = number.toString();
+	if (numAsString.length == 1) {
+		return '0' + numAsString;
+	}
+	return numAsString;
+}
+function updateCanvas(canvas, classes) {
+	var hours = 12;
+	var gX = 25, gY = 10, gsX = 200, gsY = 50;
+	var ctx = canvas.getContext('2d');
+	
+	//evenly space colors between parent classes
+	var hue = 0; //random number
+	for (var i = 0; i < classes.length; i++) {
+		var c = classes[i];
+		if (c.type == "Lecture") {
+			c.displayColor = hueVal(hue, 0.5);
+			//go through subclasses, same hue but 0.7 or something for the val
+			for (var j = 0; j < c.subClasses.length; j++) {
+				c.subClasses[j].displayColor = hueVal(hue, 0.7);
+			}
+		}
+	} 
+	
+	ctx.fillStyle = '#fff';
+	ctx.fillRect(0,0, canvas.width, canvas.height);
+	
+	//background grid
+	ctx.font = '16px sans-serif';
+	ctx.textAlign = 'right';
+    ctx.fillStyle = '#888';
+	ctx.strokeStyle = '#888';
+    ctx.beginPath();
+	for (var i = 0; i < 6; i++) {
+		ctx.moveTo(gX + i*gsX, gY);
+		ctx.lineTo(gX + i*gsX, gY + hours*gsY);
+	}
+	for (var i = 0; i <= hours; i++) {
+		ctx.moveTo(gX, gY + gsY*i);
+		ctx.lineTo(gX + 5*gsX, gY + gsY*i);
+		if (i%2 == 0) {
+			ctx.fillText(i + 8, gX - 4, gY + gsY*i + 5);
+		}
+	}
+    ctx.stroke();
+	
+	ctx.textAlign = 'left';
+	
+	for (var i = 0; i < classes.length; i++) {
+		var c = classes[i];
+		
+		var stime = c.stime[0] + (c.stime[1]/60); //start time
+		var dur = c.etime[0] + (c.etime[1]/60) - stime; //start time
+		
+		console.log(c.days);
+		for (d = 0; d < c.days.length; d++) {
+			if (c.days[d]) {
+				console.log(d);
+				ctx.fillStyle = c.displayColor;
+				
+				ctx.fillRect(gX + i*gsX + 1, gY + (stime*gsY - 8*gsY), gsX - 1, (dur*gsY));
+				
+				//shadow text
+				ctx.fillStyle = '#bbb';
+				var sOffX = 1, sOffY = 1;
+				ctx.fillText(c.major + ' ' + c.number, gX + gsX/30 + gsX*i + sOffX, gY + gsY/10 + 10 + (stime*gsY - 8*gsY) + sOffY);
+				ctx.font = '12px sans-serif';
+				ctx.fillText(c.location, gX + gsX/30 + gsX*i + sOffX, gY + gsY/10 + 10 + 10 + (stime*gsY - 8*gsY) + sOffY);
+				ctx.fillText(c.stime[0] + ':' + padMinutes(c.stime[0]) + ' - ' + c.etime[0] + ':' + padMinutes(c.etime[0]), gX + gsX/30 + gsX*i + sOffX, gY + gsY/10 + 10 + 10 + 10 + (stime*gsY - 8*gsY) + sOffY);
+				ctx.font = '16px sans-serif';
+				
+				ctx.fillStyle = '#fff';
+				ctx.fillText(c.major + ' ' + c.number, gX + gsX/30 + gsX*i, gY + gsY/10 + 10 + (stime*gsY - 8*gsY));
+				ctx.font = '12px sans-serif';
+				ctx.fillText(c.location, gX + gsX/30 + gsX*i, gY + gsY/10 + 10 + 10 + (stime*gsY - 8*gsY));
+				ctx.fillText(c.stime[0] + ':' + padMinutes(c.stime[0]) + ' - ' + c.etime[0] + ':' + padMinutes(c.etime[0]), gX + gsX/30 + gsX*i, gY + gsY/10 + 10 + 10 + 10 + (stime*gsY - 8*gsY));
+				ctx.font = '16px sans-serif';
+			}
+		}
+	}
+}
+
+//http://jsfiddle.net/wboykinm/fL0q2uce/
+function downloadCanvas(link, canvasId, filename) {
+    link.href = document.getElementById(canvasId).toDataURL();
+    link.download = filename;
 }
