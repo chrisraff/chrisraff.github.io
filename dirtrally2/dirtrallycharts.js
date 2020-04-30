92a971067dba35e251cbee36d02dc68c1aac598d
chrisraff <crharfifs@gmail.com>
2020-04-30 18:00:17 -0400

    default group by car

--- dirtrally2/dirtrallycharts.js
+++ dirtrally2/dirtrallycharts.js
@@ -1,5 +1,5 @@
 var stageData = null;
-var category = "none";
+var category = "vehicleName";
 var colors = ['#4dc9f6', '#f67019', '#f53794', '#537bc4', '#acc236', '#166a8f', '#00a950', '#58595b', '#8549ba'];
 var chartDists = null;
 var chartCount = null;
